b20fd641b6c74bd78dc73d57385248e85b7c6d17
sasha <alexmason@ukr.net>
2016-09-22 23:21:25 +0300

    main page

---
+++ footer.php
@@ -0,0 +1,16 @@
+<footer>
+			<div class="row">
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias similique suscipit voluptate sed libero distinctio obcaecati ratione, quia ut officiis animi est, consequuntur velit illum atque dignissimos neque voluptas. Omnis!</p>
+				<p>holder</p>
+				<p><ul>
+					<li><a href="#">1</a></li>
+					<li><a href="#">2</a></li>
+					<li><a href="#">3</a></li>
+					<li><a href="#">4</a></li>
+				</ul></p>
+				<?php wp_footer(); ?>
+			</div>
+		</footer>
+	</section>
+</body>
+</html>
--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php bloginfo('name'); ?></title> 
 	<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/main.css"/>
 <?php wp_head(); ?>	
@@ -10,29 +11,36 @@
 	<div class="page_wrapper">
 		<div class="header-wrapper">
 			<header>
-				<div class="logo"></div>
-				<div class="tools"></div>
-				<?php wp_nav_menu(array('theme_location' => 'nav_menu')) ?>
-					
-				
+				<div class="row">
+					<div class="col-12">
+						<div class="logo f-left"><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name') ?></a></div>
+						<div class="tools f-right">
+							<ul>
+								<li><a href="#" class="wish"></a></li>
+								<li><a href="<?php bloginfo('url'); ?>/shop/cart" class="cart"></a></li>
+								<li><a href="#" class="search"></a></li>
+							</ul>
+						</div>
+						<?php wp_nav_menu(array('theme_location' => 'nav_menu')) ?>
+						<div class="clear"></div>
+					</div>
+				</div>
 			</header>
 		</div>
+		<!-- content part-->
+		<div class="content">
+			<div class="row">
+				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; ?>
 
-		
-			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; ?>
+				<?php else: ?>
 
-			<?php else: ?>
-
-			<?php endif; ?>
-
-		
-		<footer>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias similique suscipit voluptate sed libero distinctio obcaecati ratione, quia ut officiis animi est, consequuntur velit illum atque dignissimos neque voluptas. Omnis!</p>
-			<p>holder</p>
-			<?php wp_footer(); ?>
-		</footer>
+				<?php endif; ?>
+			</div>
+		</div>
+		<!-- content part - end -->		
+		<?php wp_footer(); ?>
 	</div>
 </body>
 </html>
